8d8fd2f66beeefb4d61d8b220bce69bef620956c
MaximTestov3108 <max1testov@gmail.com>
2019-12-15 17:05:30 +0300

    First

---
+++ oof/TicTacToe.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oof
+{
+    public partial class TicTacToe : Form
+    {
+        enum Players
+        {
+            Not,
+            Cross,
+            Zero
+        }
+
+        Players last_player = Players.Zero;
+        Players[] Boxes = new Players[9];
+        Players win = Players.Not;
+        private void players_replace(Button cur_button, int pos)
+        {
+            if (last_player == Players.Cross)
+            {
+                var bmp = new Bitmap(Properties.Resources.пончик);
+                cur_button.BackgroundImage = bmp;
+                cur_button.BackgroundImageLayout = ImageLayout.Stretch;
+                last_player = Players.Zero;
+                changes(pos, Players.Zero);
+            } else if (last_player == Players.Zero)
+            {
+                var bmp = new Bitmap(Properties.Resources.салат);
+                cur_button.BackgroundImage = bmp;
+                cur_button.BackgroundImageLayout = ImageLayout.Stretch;
+                last_player = Players.Cross;
+                changes(pos, Players.Cross);
+            }
+            cur_button.Enabled = false;
+
+            end_game();
+            if 
+        }
+
+        private void end_game()
+        {
+            if (Boxes[0].Equals(Boxes[1]) && Boxes[0].Equals(Boxes[2]) && !Boxes[0].Equals(Players.Not))
+            {
+                win = Boxes[0];
+            } else if (Boxes[3].Equals(Boxes[4]) && Boxes[3].Equals(Boxes[5]) && !Boxes[3].Equals(Players.Not))
+            {
+                win = Boxes[3];
+            } else if (Boxes[6].Equals(Boxes[7]) && Boxes[6].Equals(Boxes[8]) && !Boxes[6].Equals(Players.Not))
+            {
+                win = Boxes[6];
+            } else if (Boxes[0].Equals(Boxes[4]) && Boxes[0].Equals(Boxes[8]) && !Boxes[0].Equals(Players.Not))
+            {
+                win = Boxes[0];
+            } else if (Boxes[2].Equals(Boxes[4]) && Boxes[2].Equals(Boxes[6]) && !Boxes[2].Equals(Players.Not))
+            {
+                win = Boxes[2];
+            }
+        }
+
+        private void changes(int pos, Players player)
+        {
+
+            Boxes[pos] = player;
+        }
+
+        public TicTacToe()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            players_replace(button1, 0);
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            players_replace(button5, 1);
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            players_replace(button4, 2);
+        }
+
+        private void Button9_Click(object sender, EventArgs e)
+        {
+            players_replace(button9, 3);
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            players_replace(button6, 4);
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            players_replace(button3, 5);
+        }
+
+        private void Button8_Click(object sender, EventArgs e)
+        {
+            players_replace(button8, 6);
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            players_replace(button7, 7);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            players_replace(button2, 8);
+        }
+    }
+}
---
+++ oof/Win.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace oof
+{
+    partial class Win
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.richTextBox1.Location = new System.Drawing.Point(76, 29);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(661, 96);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "Победа одного из вас!";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.RichTextBox1_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::oof.Properties.Resources._300px_Звуки_счастья_шаблон;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(92, 145);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(661, 238);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Win
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Win";
+            this.Text = "Win";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
